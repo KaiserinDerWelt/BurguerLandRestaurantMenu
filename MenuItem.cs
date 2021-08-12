a171a6abebbd54a44ef1efe8a57e727b7be9d75b
Mariana Licona <m.licona@abtis.de>
2021-08-12 17:27:26 +0200

    introduce MenuItem class

---
+++ MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurguerLandRestaurantMenu
+{
+    class MenuItem
+    {
+    }
+}
